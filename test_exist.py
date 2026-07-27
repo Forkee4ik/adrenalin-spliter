cc15cf2542496ff1cea588b42b98cc382066fbe8
Forkee4ik <01anatoly09@gmail.com>
2026-07-27 17:49:19 +0300

    Delete test_exist.py

--- test_exist.py
+++
@@ -1,7 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-import os
-import subprocess
-video = r"E:\запись\unknown\unknown_replay_2026.07.27-15.52.mp4"
-audio = r"E:\запись\unknown\unknown_replay_2026.07.27-15.52.m4a"
-print("Video exists:", os.path.exists(video))
-print("Audio exists:", os.path.exists(audio))
